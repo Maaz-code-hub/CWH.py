c45746be46d58d55a17d6f634b1e5dc204df8254
Maaz-code-hub <kolemaaz6@gmail.com>
2024-12-01 17:08:46 +0530

    Harry bhai's python course

---
+++ 015-Exercise 2 Good Morning Sir/question.py
@@ -0,0 +1,17 @@
+# Create a python program capable of greeting you with Good Morning, Good Afternoon and Good Evening. Your program should use time module to get the current hour. Here is a sample program and documentation link for you:
+# Eg
+import time
+timestamp = time.strftime('%H:%M:%S')
+print(timestamp)
+timestamp = int(time.strftime('%H'))
+print(timestamp)
+timestamp = int(time.strftime('%M'))
+print(timestamp)
+timestamp = int(time.strftime('%S'))
+print(timestamp)
+# https://docs.python.org/3/library/time.html#time.strftime
+
+
+
+
+
