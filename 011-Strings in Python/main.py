da25dbebe22178f68d075873e4002612f20c7dc5
Maaz-code-hub <kolemaaz6@gmail.com>
2024-12-02 23:22:31 +0530

    Harry bhai's python course

--- 011-Strings in Python/main.py
+++ 011-Strings in Python/main.py
@@ -1,12 +1,4 @@
-# what is string
-# In python, anything that you enclose between single or double quotation marks is considered a string. A string is essentially a sequence or array of textual data. Strings are used when working with Unicode charact
-
 name="maaz"
-
-# How will you print this statement in python?: He said, "I want to eat an apple". We will definitely use single quotes for our convenience
-
-# we usually use single quotes('xyz')when we have a statement consisting of double quote("xyz") and vise versa(we can also use \' or \")
-# example
 
 print("He said, 'I want to eat an apple'.")
 print('He said, "I want to eat an apple".')
