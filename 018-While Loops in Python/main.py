4e65f91b1c83e15c101548931da0749234cad346
Maaz-code-hub <kolemaaz6@gmail.com>
2024-12-03 19:48:11 +0530

    Harry bhai's python course

---
+++ 018-While Loops in Python/main.py
@@ -0,0 +1,26 @@
+# i = int(input("Enter the number : "))
+# while(i<=38):
+#     i = int(input("Enter the number : "))
+#     print(i)
+
+# print("Done with the loop")
+
+
+count = 5
+while (count > 0):
+  print(count)
+  count = count - 1
+else:
+  print("I am inside else")
+
+
+  # Do while loop 
+  i=0
+  while True:
+    print(i)
+    i = i+1
+    if(i%100 == 0):
+      break
+
+
+
---
+++ 019-Break and continue in Python/main.py
@@ -0,0 +1,22 @@
+for i in range(1,15):
+    if(i ==10):
+        break
+    print(i)
+
+
+print("\n")
+
+
+for i in range(1,15):
+    if(i ==10):
+        continue
+    print(i)
+
+
+# for i in range(1,101,1):
+#     print(i ,end=" ")
+#     if(i==50):
+#         break
+#     else:
+#         print("Mississippi")
+# print("Thank you")
