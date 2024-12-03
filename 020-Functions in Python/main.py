4bc5bd4e21ab3ccfcecfb3b0d19eac2eacb473ec
Maaz-code-hub <kolemaaz6@gmail.com>
2024-12-03 20:18:54 +0530

    Harry bhai's python course

---
+++ 020-Functions in Python/main.py
@@ -0,0 +1,34 @@
+def mean(a,b):
+    mean=(a*b)/(2)
+    print(mean)
+
+def Greater(a,b):
+    if(a>b):
+        print("A is greater",a,">",b)
+    else:
+        print("B is greater",b,">",a)
+
+def lesser(a,b):
+    pass
+
+a=9
+b=8
+# mean1=(a*b)/(a+b)
+# if(a>b):
+#     print("A is greater",a,">",b)
+# else:
+#     print("B is greater",b,">",a)
+# print(mean1)
+mean(a,b)
+Greater(a,b)
+
+c=8
+d=7
+# mean2=(c*d)/(c+d)
+# if(a>b):
+#     print("A is greater",a,">",b)
+# else:
+#     print("B is greater",b,">",a)
+# print(mean2)
+mean(c,d)
+Greater(c,d)
